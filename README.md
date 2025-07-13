cae6690566246e8649e43bca2a308ce19fe40313
Nocnica Mellifera <noctnica@gmail.com>
2025-07-12 17:21:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Checkly Uptime Monitoring Demo
+
+This example project shows how you can use the Checkly CLI in a monitoring as code (MaC) workflow to perform uptime monitoring
---
+++ checkly.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'checkly'
+
+/**
+ * See https://www.checklyhq.com/docs/cli/project-structure/
+ */
+const config = defineConfig({
+  /* A human friendly name for your project */
+  projectName: 'Uptime Monitoring Demo',
+  /** A logical ID that needs to be unique across your Checkly account,
+  * See https://www.checklyhq.com/docs/cli/constructs/ to learn more about logical IDs.
+  */
+  logicalId: 'uptime-monitoring-1',
+  /* An optional URL to your Git repo */
+  repoUrl: 'https://github.com/serverless-mom/uptimeMonitoring',
+  /* Sets default values for Checks */
+  checks: {
+    /* A default for how often your Check should run in minutes */
+    frequency: 10,
+    /* Checkly data centers to run your Checks as monitors */
+    locations: ['us-east-1', 'eu-west-1'],
+    /* An optional array of tags to organize your Checks */
+    tags: ['uptime'],
+    /** The Checkly Runtime identifier, determining npm packages and the Node.js version available at runtime.
+     * See https://www.checklyhq.com/docs/cli/npm-packages/
+     */
+    runtimeId: '2025.04',
+    /* A glob pattern that matches the Checks inside your repo, see https://www.checklyhq.com/docs/cli/using-check-test-match/ */
+    checkMatch: '**/__checks__/**/*.check.ts',
+    /* Global configuration option for Playwright-powered checks. See https://www.checklyhq.com/docs/browser-checks/playwright-test/#global-configuration */
+    playwrightConfig: {},
+    browserChecks: {
+
+      testMatch: '**/__checks__/**/*.spec.ts',
+    },
+  },
+  cli: {
+    /* The default datacenter location to use when running npx checkly test */
+    runLocation: 'eu-west-1',
+    /* An array of default reporters to use when a reporter is not specified with the "--reporter" flag */
+    reporters: ['list'],
+    /* How many times to retry a failing test run when running `npx checkly test` or `npx checkly trigger` (max. 3) */
+    retries: 0,
+  },
+})
+
+export default config
